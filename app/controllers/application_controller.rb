d8be3fec285957969a867d1313e5e9c3da62b0fb
abeidahmed <abeidahmed92@gmail.com>
2020-05-23 09:32:13 +0530

    Refactor code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,12 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
-  
+
+  include ApplicationHelper
+
   def encode_credential_for(user)
     secret_key = Rails.application.secrets.secret_key_base
     data_to_encode = { user_id: "#{user.id}" }
     token = JWT.encode data_to_encode, secret_key, "HS256"
     @hash = { token: token }
-  end
-
-  def current_user
-    header = request.headers["Authorization"]
-    unless header.blank?
-      token = header.sub!("Bearer ", "")
-      secret_key = Rails.application.secrets.secret_key_base
-      decoded_token = JWT.decode header, secret_key, true, { algorithm: 'HS256' }
-      id = decoded_token[0]["user_id"]
-      User.find_by(id: id)
-    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def current_user
+    header = request.headers["Authorization"]
+    unless header.blank?
+      token = header.sub!("Bearer ", "")
+      secret_key = Rails.application.secrets.secret_key_base
+      decoded_token = JWT.decode header, secret_key, true, { algorithm: 'HS256' }
+      id = decoded_token[0]["user_id"]
+      User.find_by(id: id)
+    end
+  end
 end
