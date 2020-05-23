38176011bf6d47b24f43d18bfcc99698e9df67fa
abeidahmed <abeidahmed92@gmail.com>
2020-05-23 22:35:27 +0530

    Make login form interact with backend

--- app/javascript/packs/views/login/components/form.js
+++ app/javascript/packs/views/login/components/form.js
@@ -1,13 +1,46 @@
 import React, { useState } from "react";
+import axios from "axios";
 import Icon from "@components/icon";
 import { Link } from "react-router-dom";
 
 const Form = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const loginUser = () => {
+    axios
+      .post(
+        "/api/v1/sessions",
+        {
+          user: {
+            email,
+            password
+          }
+        },
+        { withCredentials: true }
+      )
+      .then(res => {
+        if (res.status === 200) {
+          console.log(res);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        setError(err.response.data.message);
+        setIsLoading(false);
+      });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setIsLoading(true);
+    loginUser();
+  };
 
   return (
-    <form className="mt-8">
+    <form className="mt-8" onSubmit={handleSubmit}>
       <div className="rounded-md shadow-sm">
         <div>
           <input
@@ -32,6 +65,7 @@ const Form = () => {
       </div>
 
       <div className="mt-6 flex items-center justify-between">
+        {error && <p className="text-sm font-medium text-red-700">{error}</p>}
         <div className="ml-auto text-sm leading-5">
           <Link
             to="/"
@@ -44,6 +78,7 @@ const Form = () => {
 
       <div className="mt-6">
         <button
+          disabled={isLoading}
           type="submit"
           className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
         >
@@ -53,7 +88,7 @@ const Form = () => {
               className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150"
             />
           </span>
-          Sign in
+          {isLoading ? "Signing in..." : "Sign in"}
         </button>
       </div>
     </form>
