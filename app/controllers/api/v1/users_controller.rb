84e5f3a7184a8deb8e56231a800d23ca41e40e2c
abeidahmed <abeidahmed92@gmail.com>
2020-05-23 13:59:17 +0530

    Whitelist role param

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -16,6 +16,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :role)
   end
 end
