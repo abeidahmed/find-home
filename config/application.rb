11384a1f4376f4e8daa3dedd9c7759e3c3b82edb
abeidahmed <abeidahmed92@gmail.com>
2020-05-23 09:03:02 +0530

    Skip assets and tests

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,11 @@ module FindHome
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    #disable asset generation
+    config.generators do |g|
+      g.assets false
+      g.test_framework nil
+    end
   end
 end
