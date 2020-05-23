974e5bceb9a3212778d5490113a72b06d43001d2
abeidahmed <abeidahmed92@gmail.com>
2020-05-23 09:08:41 +0530

    Add jwt hash method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  def encode_credential_for(user)
+    secret_key = Rails.application.secrets.secret_key_base
+    data_to_encode = { user_id: "#{user.id}" }
+    token = JWT.encode data_to_encode, secret_key, "HS256"
+    @hash = { token: token }
+  end
 end
