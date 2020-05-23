56940ff09b0e3f7f02872ed6ee13bef7f35719d7
abeidahmed <abeidahmed92@gmail.com>
2020-05-23 09:23:27 +0530

    Add current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,15 @@ class ApplicationController < ActionController::Base
     token = JWT.encode data_to_encode, secret_key, "HS256"
     @hash = { token: token }
   end
+
+  def current_user
+    header = request.headers["Authorization"]
+    unless header.blank?
+      token = header.sub!("Bearer ", "")
+      secret_key = Rails.application.secrets.secret_key_base
+      decoded_token = JWT.decode header, secret_key, true, { algorithm: 'HS256' }
+      id = decoded_token[0]["user_id"]
+      User.find_by(id: id)
+    end
+  end
 end
