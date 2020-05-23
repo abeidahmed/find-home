3591d3b4022b1d087ace6b1343361e8f61bed3ca
abeidahmed <abeidahmed92@gmail.com>
2020-05-23 09:04:31 +0530

    Fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  namespace :admin, defaults: { format: :json } do
-    namespace :api do
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
       
     end
   end
