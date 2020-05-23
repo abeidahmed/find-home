3693fad99d17a7f3c82e403a81e930fa23a30ca0
abeidahmed <abeidahmed92@gmail.com>
2020-05-23 22:26:35 +0530

    Add error class

--- app/javascript/packs/components/field/input-field.js
+++ app/javascript/packs/components/field/input-field.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { fieldValidation } from "utils/field-validation";
+import { fieldValidation } from "@utils/field-validation";
 
 export const InputField = ({ label, id, error, errorType, ...props }) => {
   return (
@@ -7,10 +7,15 @@ export const InputField = ({ label, id, error, errorType, ...props }) => {
       <label htmlFor={id} className="text-sm text-gray-700 font-medium">
         {label}
       </label>
-      <input id={id} className={`form-input mt-1 block w-full px-3 py-2 shadow`} {...props} />
-      {/* {fieldValidation(error, errorType) && (
+      <input
+        id={id}
+        className={`form-input ${fieldValidation(error, errorType) &&
+          "form-input-error"} mt-1 block w-full px-3 py-2 shadow`}
+        {...props}
+      />
+      {fieldValidation(error, errorType) && (
         <p className="mt-1 text-sm font-medium text-red-700">{fieldValidation(error, errorType)}</p>
-      )} */}
+      )}
     </>
   );
 };
