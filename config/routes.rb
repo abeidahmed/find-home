d1c355d34467de189b5c57942f9e3331891c7b26
abeidahmed <abeidahmed92@gmail.com>
2020-05-23 08:58:01 +0530

    Add default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin, defaults: { format: :json } do
+    namespace :api do
+      
+    end
+  end
+
+  get "/*path", to: "root#root"
 end
