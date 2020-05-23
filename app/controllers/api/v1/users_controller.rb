865ab4068063b0ef53f289020125de11a0fb6937
abeidahmed <abeidahmed92@gmail.com>
2020-05-23 09:09:01 +0530

    Add create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::UsersController < ApplicationController
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      encode_credential_for(@user)
+      
+      render :new, status: :created
+    else
+      render json: @user.errors.full_messages, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 end
