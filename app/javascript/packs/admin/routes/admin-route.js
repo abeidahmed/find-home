51a0bda691363382b41312c9371070203e9b463c
abeidahmed <abeidahmed92@gmail.com>
2020-05-24 22:45:19 +0530

    Use match param

--- app/javascript/packs/admin/routes/admin-route.js
+++ app/javascript/packs/admin/routes/admin-route.js
@@ -5,15 +5,15 @@ import Header from "@admin/views/header";
 import Sidebar from "@admin/views/sidebar";
 import UserList from "@admin/views/user/user-list";
 
-const AdminRoute = () => {
+const AdminRoute = ({ match }) => {
   return (
     <div className="flex">
       <Sidebar />
       <div className="w-full flex flex-col">
         <Header />
         <Switch>
-          <Route path="/admin/dashboard" component={Dashboard} />
-          <Route path="/admin/users" component={UserList} />
+          <Route path={`${match.path}/dashboard`} component={Dashboard} />
+          <Route path={`${match.path}/users`} component={UserList} />
         </Switch>
       </div>
     </div>
