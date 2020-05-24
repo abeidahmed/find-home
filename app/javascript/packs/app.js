83ec875ed5c21e26a8efc676bdebb434359c7707
abeidahmed <abeidahmed92@gmail.com>
2020-05-24 16:23:08 +0530

    Check for current user

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "@/store";
+import { fetchCurrentUser } from "@/api/current-user";
 import PublicRoute from "@/routes/public-route";
 
 const App = () => {
+  useEffect(() => {
+    if (store.getState().currentUser.token !== null) {
+      store.dispatch(fetchCurrentUser());
+    }
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
