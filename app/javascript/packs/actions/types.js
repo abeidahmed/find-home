b384650f49ac23f42871e38c1576edec62880ec2
abeidahmed <abeidahmed92@gmail.com>
2020-05-24 21:32:08 +0530

    Add user fetch types

--- app/javascript/packs/actions/types.js
+++ app/javascript/packs/actions/types.js
@@ -4,3 +4,7 @@ export const POST_CURRENT_USER = "POST_CURRENT_USER";
 // sidebar
 export const OPEN_SIDEBAR = "OPEN_SIDEBAR";
 export const CLOSE_SIDEBAR = "CLOSE_SIDEBAR";
+
+// user list
+export const FETCH_USERS = "FETCH_USERS";
+export const FETCH_USERS_LOADING = "FETCH_USERS_LOADING";
