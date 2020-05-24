c8a10beca64a291fb991144bbaabede8760653cf
abeidahmed <abeidahmed92@gmail.com>
2020-05-24 16:31:03 +0530

    Dynamically render profile links

--- app/javascript/packs/views/header/components/desktop-links.js
+++ app/javascript/packs/views/header/components/desktop-links.js
@@ -1,9 +1,10 @@
 import React, { useState, useRef } from "react";
 import { AvatarWithButton } from "@components/avatar";
+import { connect } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
 import { links, profileLinks } from "./links";
 
-const DesktopLinks = () => {
+const DesktopLinks = ({ currentUser }) => {
   const [profileActive, setProfileActive] = useState(false);
 
   return (
@@ -18,53 +19,64 @@ const DesktopLinks = () => {
         </Link>
       ))}
 
-      <div className="relative">
-        <AvatarWithButton toggleDropdown={setProfileActive} dropdownState={profileActive} />
+      {currentUser ? (
+        <div className="relative">
+          <AvatarWithButton toggleDropdown={setProfileActive} dropdownState={profileActive} />
 
-        <div
-          className={`${
-            profileActive ? "block" : "hidden"
-          } origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg`}
-        >
           <div
-            className="py-1 rounded-md bg-white shadow-xs"
-            role="menu"
-            aria-orientation="vertical"
-            aria-labelledby="user-menu"
+            className={`${
+              profileActive ? "block" : "hidden"
+            } origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg`}
           >
-            {profileLinks.map(profileLink => (
-              <Link
-                key={profileLink.title}
-                to={profileLink.path}
-                className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
-              >
-                {profileLink.title}
-              </Link>
-            ))}
-            <button className="block w-full text-left px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
-              Sign out
-            </button>
+            <div
+              className="py-1 rounded-md bg-white shadow-xs"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="user-menu"
+            >
+              {profileLinks.map(profileLink => (
+                <Link
+                  key={profileLink.title}
+                  to={profileLink.path}
+                  className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+                >
+                  {profileLink.title}
+                </Link>
+              ))}
+              <button className="block w-full text-left px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
+                Sign out
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <>
+          <Link
+            to="/login"
+            className="font-medium text-indigo-600 hover:text-indigo-900 focus:outline-none focus:text-indigo-700 transition duration-150 ease-in-out"
+          >
+            Log in
+          </Link>
 
-      <>
-        <Link
-          to="/login"
-          className="font-medium text-indigo-600 hover:text-indigo-900 focus:outline-none focus:text-indigo-700 transition duration-150 ease-in-out"
-        >
-          Log in
-        </Link>
-
-        <Link
-          to="/signup"
-          className="inline-flex items-center justify-center rounded-md shadow px-5 py-2 border border-transparent leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
-        >
-          Sign up
-        </Link>
-      </>
+          <Link
+            to="/signup"
+            className="inline-flex items-center justify-center rounded-md shadow px-5 py-2 border border-transparent leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+          >
+            Sign up
+          </Link>
+        </>
+      )}
     </nav>
   );
 };
 
-export default DesktopLinks;
+const mapStateToProps = state => {
+  return {
+    currentUser: state.currentUser.user
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(DesktopLinks);
