e22baf7f9501e1bf81f4d3833c4733b9c078d45c
abeidahmed <abeidahmed92@gmail.com>
2020-05-24 20:01:04 +0530

    Fix icon

--- app/javascript/packs/admin/views/sidebar/index.js
+++ app/javascript/packs/admin/views/sidebar/index.js
@@ -12,7 +12,7 @@ const Sidebar = () => {
           <img src={LogoFull} className="h-8 w-auto" alt="Find home" />
         </Link>
         <button className="lg:hidden p-1 -mr-2 rounded-md text-gray-700 focus:outline-none focus:text-gray-900 focus:bg-gray-100 transition duration-150 ease-in-out">
-          <Icon icon="menu" className="h-6 w-6" />
+          <Icon icon="close" className="h-6 w-6" />
         </button>
       </div>
       <div className="flex-1 overflow-y-auto">
