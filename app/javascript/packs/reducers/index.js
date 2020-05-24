dcef160ed455445573e88ff9bc9293238337b205
abeidahmed <abeidahmed92@gmail.com>
2020-05-24 21:32:54 +0530

    Add user list reducer

--- app/javascript/packs/reducers/index.js
+++ app/javascript/packs/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { currentUser } from "./current-user";
 import { sidebar } from "./sidebar";
+import { userList } from "./user-list";
 
 export const rootReducer = combineReducers({
   currentUser,
-  sidebar
+  sidebar,
+  userList
 });
