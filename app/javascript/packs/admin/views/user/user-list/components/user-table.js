feff82e28eca180f13898454659444cca7b0824e
abeidahmed <abeidahmed92@gmail.com>
2020-05-25 21:39:33 +0530

    Use user avatar instead of static images

--- app/javascript/packs/admin/views/user/user-list/components/user-table.js
+++ app/javascript/packs/admin/views/user/user-list/components/user-table.js
@@ -30,7 +30,7 @@ const UserTable = ({ users }) => {
                     <div className="flex-shrink-0 h-10 w-10">
                       <img
                         className="h-10 w-10 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src={user.avatar}
                         alt={user.fullName}
                       />
                     </div>
