e584aa0ea9c77978495309bbaf0c05e718895e37
abeidahmed <abeidahmed92@gmail.com>
2020-05-25 21:38:49 +0530

    Add gravatar helper method

--- app/helpers/api/v1/users_helper.rb
+++ app/helpers/api/v1/users_helper.rb
@@ -1,2 +1,9 @@
 module Api::V1::UsersHelper
+  # Return the link of the user's gravatar
+  def gravatar_for(user, options = { size: 80, default_avatar: "mp" })
+    size = options[:size]
+    default_avatar = options[:default_avatar]
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=#{default_avatar}"
+  end
 end
