4eecdb622747e419de1a61120a93086a6b3e9e45
abeidahmed <abeidahmed92@gmail.com>
2020-05-26 18:25:31 +0530

    Add modal reducer to main

--- app/javascript/packs/reducers/index.js
+++ app/javascript/packs/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import { currentUser } from "./current-user";
+import { modal } from "./modal";
 import { sidebar } from "./sidebar";
 import { userList } from "./user-list";
 
 export const rootReducer = combineReducers({
   currentUser,
+  modal,
   sidebar,
   userList
 });
