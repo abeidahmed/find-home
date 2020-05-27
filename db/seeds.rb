202653f108932d7ca8810f3fe1f855e0192ee514
abeidahmed <abeidahmed92@gmail.com>
2020-05-27 21:38:24 +0530

    Add tag seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,12 @@
 
 User.destroy_all
 Category.destroy_all
+Tag.destroy_all
 
 20.times do
   User.create!(
     first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "secret"
   )
   Category.create!(title: Faker::Book.unique.genre, description: Faker::Quote.famous_last_words)
+  Tag.create!(title: Faker::Music.unique.band, description: Faker::Music.album)
 end
