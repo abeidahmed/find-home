86942cd3251d2161a9ae68cacfbd960f5686ae1c
abeidahmed <abeidahmed92@gmail.com>
2020-05-27 14:19:59 +0530

    Add category seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
+Category.destroy_all
 
 20.times do
   User.create!(
     first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "secret"
   )
+  Category.create!(title: Faker::Book.unique.genre, description: Faker::Quote.famous_last_words)
 end
