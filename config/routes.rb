88fae8e7f15b81324067ea2e44aa6e1d1bdce188
abeidahmed <abeidahmed92@gmail.com>
2020-05-27 17:46:12 +0530

    Add destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       resources :users, only: [:index, :create, :destroy]
       resources :sessions, only: [:create]
       resources :categories, only: [:index, :create, :show, :update, :destroy]
-      resources :posts, only: [:create]
+      resources :posts, only: [:create, :destroy]
 
       get "/logged_in", to: "sessions#logged_in"
     end
