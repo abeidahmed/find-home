008c84cefec918269d63205732964624a4740146
abeidahmed <abeidahmed92@gmail.com>
2020-05-27 17:41:44 +0530

    Add create route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :users, only: [:index, :create, :destroy]
       resources :sessions, only: [:create]
       resources :categories, only: [:index, :create, :show, :update, :destroy]
+      resources :posts, only: [:create]
 
       get "/logged_in", to: "sessions#logged_in"
     end
