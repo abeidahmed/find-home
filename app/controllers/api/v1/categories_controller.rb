7a1e90e50fdebf2131dce5dc66a6c11faef33da8
abeidahmed <abeidahmed92@gmail.com>
2020-05-27 14:02:39 +0530

    Add create action

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::CategoriesController < ApplicationController
+  before_action :check_authorization, only: [:create]
+  
+  def create
+    @category = Category.create(category_params)
+    if @category.save
+      render :new, status: :created
+    else
+      render json: @category.errors.full_messages, status: :bad_request
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:title, :description)
+  end
 end
