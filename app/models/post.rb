dfe8c13479801ecb8ac30b598662060ab264bea8
abeidahmed <abeidahmed92@gmail.com>
2020-05-27 17:30:58 +0530

    Add post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :title, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
+  validates :content, presence: true
+  validates :excerpt, length: { maximum: 500 }
+
+  default_scope -> { order(created_at: :desc) }
+
+  self.per_page = 5
 end
