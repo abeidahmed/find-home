ac98526b0a55c99007132d0168f46c4a574a577e
abeidahmed <abeidahmed92@gmail.com>
2020-05-27 14:02:09 +0530

    Add uniqueness validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 255 }
+  validates :title, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
   validates :description, length: { in: 10..500 }
 
   default_scope -> { order(created_at: :desc) }
