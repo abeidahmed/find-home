a3ceb1b01a20a950d6f43c1266ad9b86c5421338
abeidahmed <abeidahmed92@gmail.com>
2020-05-27 21:18:00 +0530

    Add tag validation

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,8 @@
 class Tag < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: true }
+  validates :description, length: { maximum: 250 }
+
+  default_scope -> { order(created_at: :desc) }
+
+  self.per_page = 5
 end
