5ab68b59ff9e78f35e660bcc00e32dcd0d16914d
abeidahmed <abeidahmed92@gmail.com>
2020-05-27 17:41:57 +0530

    Add create action

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::PostsController < ApplicationController
+  def create
+    @post = current_user.posts.create(post_params)
+    if @post.save
+      render :new, status: :created
+    else
+      render json: @post.errors.full_messages, status: :bad_request
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :excerpt, :category_id)
+  end
 end
