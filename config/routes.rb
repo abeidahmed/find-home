1a4e4f432a661960d99bc6ef0a1efe9b101d70c4
abeidahmed <abeidahmed92@gmail.com>
2020-05-27 15:03:04 +0530

    Add show action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :create, :destroy]
       resources :sessions, only: [:create]
-      resources :categories, only: [:index, :create, :update, :destroy]
+      resources :categories, only: [:index, :create, :show, :update, :destroy]
 
       get "/logged_in", to: "sessions#logged_in"
     end
