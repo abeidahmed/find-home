bd4b7b23c402d85b4602100abaae3225cc120e7c
abeidahmed <abeidahmed92@gmail.com>
2020-05-27 13:46:04 +0530

    Add category validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :description, length: { in: 10..500 }
+
+  default_scope -> { order(created_at: :desc) }
 end
