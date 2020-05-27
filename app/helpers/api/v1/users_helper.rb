50d7a94227998e6e7c99223557010de223a3e1f4
abeidahmed <abeidahmed92@gmail.com>
2020-05-27 15:27:55 +0530

    Avoid checking lowercase email since it is already checked when user signs up

--- app/helpers/api/v1/users_helper.rb
+++ app/helpers/api/v1/users_helper.rb
@@ -3,7 +3,7 @@ module Api::V1::UsersHelper
   def gravatar_for(user, options = { size: 80, default_avatar: "mp" })
     size = options[:size]
     default_avatar = options[:default_avatar]
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_id = Digest::MD5::hexdigest(user.email)
     "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=#{default_avatar}"
   end
 end
