92cb2891409c44efac2b46758dd6f7204476084e
abeidahmed <abeidahmed92@gmail.com>
2020-05-28 23:30:05 +0530

    Add show category boilerplate

--- app/javascript/packs/admin/routes/admin-route.js
+++ app/javascript/packs/admin/routes/admin-route.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import CategoryList from "@admin/views/category/category-list";
 import Dashboard from "@admin/views/dashboard";
 import Header from "@admin/views/header";
+import ShowCategory from "@admin/views/category/show-category";
 import Sidebar from "@admin/views/sidebar";
 import UserList from "@admin/views/user/user-list";
 
@@ -15,7 +16,8 @@ const AdminRoute = ({ match }) => {
         <Switch>
           <Route path={`${match.path}/dashboard`} component={Dashboard} />
           <Route path={`${match.path}/users`} component={UserList} />
-          <Route path={`${match.path}/categories`} component={CategoryList} />
+          <Route exact path={`${match.path}/categories`} component={CategoryList} />
+          <Route path={`${match.path}/categories/:id`} component={ShowCategory} />
         </Switch>
       </div>
     </div>
