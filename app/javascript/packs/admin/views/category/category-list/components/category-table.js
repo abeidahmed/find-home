e3438c7007ac2a39f37f1c3c0f69192d478ff3a2
abeidahmed <abeidahmed92@gmail.com>
2020-05-28 23:29:52 +0530

    Add link to show category

--- app/javascript/packs/admin/views/category/category-list/components/category-table.js
+++ app/javascript/packs/admin/views/category/category-list/components/category-table.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
+import Icon from "@components/icon";
+import { Link } from "react-router-dom";
 import { openModal } from "@actions/modal";
 
 const CategoryTable = ({ categories, openModal }) => {
@@ -27,8 +29,14 @@ const CategoryTable = ({ categories, openModal }) => {
           <tbody className="bg-white">
             {categories.map(category => (
               <tr key={category.id} className="border-b border-gray-200">
-                <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900 font-medium">
-                  {category.title}
+                <td className="px-6 py-4 whitespace-no-wrap">
+                  <Link
+                    to={`/admin/categories/${category.id}`}
+                    className="flex items-center text-sm leading-5 text-gray-900 font-medium hover:text-gray-600"
+                  >
+                    {category.title}
+                    <Icon icon="link" className="h-5 w-5 text-gray-500 pl-1" />
+                  </Link>
                 </td>
                 <td className="px-6 py-4 truncate max-w-xs whitespace-no-wrap text-sm leading-5 text-gray-900">
                   {category.description}
