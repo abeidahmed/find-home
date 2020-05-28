e9a1d32dbabef3bd2fb89722aa7af27600e4ba06
abeidahmed <abeidahmed92@gmail.com>
2020-05-28 19:19:45 +0530

    Parse query param

--- app/javascript/packs/api/category/category-list.js
+++ app/javascript/packs/api/category/category-list.js
@@ -1,10 +1,21 @@
 import axios from "axios";
 import { authToken } from "@/middleware/auth-token";
 import { fetchCategories } from "@actions/category-list";
+import queryString from "query-string";
 
-export const fetchAllCategories = () => async (dispatch, getState) => {
+export const fetchAllCategories = param => async (dispatch, getState) => {
+  const url = queryString.stringifyUrl(
+    {
+      url: "/api/v1/categories",
+      query: {
+        page: param.page
+      }
+    },
+    { skipEmptyString: true }
+  );
+
   try {
-    const res = await axios.get("/api/v1/categories", authToken(getState));
+    const res = await axios.get(url, authToken(getState));
     await dispatch(fetchCategories(res.data.categories, res.data.pageInfo));
   } catch (err) {
     console.log(err.response.data);
